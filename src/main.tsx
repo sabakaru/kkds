f030c199905f5d01df1f48ffd9c1fc103f0e9a0c
sabakaru <107185107+sabakaru@users.noreply.github.com>
2024-01-23 13:41:58 +0300

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,11 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
-
+import { WebAppProvider } from '@vkruglikov/react-telegram-web-app'
 ReactDOM.createRoot(document.getElementById('root')!).render(
+  <WebAppProvider>
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
+  </React.StrictMode>
+  </WebAppProvider>,
 )
