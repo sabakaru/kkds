909a2082d020efdaf3a1f9491d46c52d93019bd9
sabakaru <michailnovikov2005@gmail.com>
2024-01-23 13:25:08 +0300

    mmm

--- src/App.tsx
+++ src/App.tsx
@@ -2,35 +2,18 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-
-import WebApp from '@twa-dev/sdk'
-
-WebApp.ready();
+import { WebAppProvider, useInitData} from '@vkruglikov/react-telegram-web-app'
 function App() {
-  const [count, setCount] = useState(0)
-
+  // Assuming you have a function that fetches a user and returns a Promise
+  
+  const [initData] = useInitData();
+  const userid = initData?.user?.id
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-      </div>
-        {/* Here we add our button with alert callback */}
-      <div className="card">
-        <button onClick={() => WebApp.showAlert(`Hello World! Current count is ${count}`)}>
-            Show Alert
-        </button>
-      </div>
+    <WebAppProvider>
+    
+<p>{userid}</p>
+    </WebAppProvider>
     </>
   )
 }
